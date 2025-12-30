15c0e1bbf6287739e2e501ac86fec9b55a6fa357
Jawad Hamdan <eng.jawad121@hotmail.com>
2025-12-31 00:33:15 +0200

    read from the sqlite DB

--- CRUD-APP/MainPage.xaml.cs
+++ CRUD-APP/MainPage.xaml.cs
@@ -1,15 +1,35 @@
 ﻿using CRUD_APP.Data;
-
+using System.Collections.ObjectModel;
+using CRUD_APP.Models;
+using Microsoft.EntityFrameworkCore;
 namespace CRUD_APP
 {
     public partial class MainPage : ContentPage
     {
-        private ApplicationDbContext _context;
-        public MainPage(ApplicationDbContext context)
+        private readonly ApplicationDbContext _context;
+        public ObservableCollection<User> Users { get; set; }
+        public  MainPage(ApplicationDbContext context)
         {
-            this._context = context;
             InitializeComponent();
+            this._context = context;
+            Users = new ObservableCollection<User>();
+            BindingContext = this;
+            LoadDataAsync();
         }
 
+        public async void LoadDataAsync()
+        {
+            try
+            {
+                await _context.Database.EnsureCreatedAsync();
+                var users = await _context.Users.ToListAsync();
+                foreach(var user in users)
+                    Users.Add(user);
+            }
+            catch(Exception ex)
+            {
+                DisplayAlert("Error",$"failed to load users {ex.Message}","OK");
+            }
+        }
     }
 }
